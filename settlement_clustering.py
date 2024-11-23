1796865fe6f59a8e31e00415369824c0f387945e
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2024-11-23 17:40:13 +0200

    Implement the visualization function
    And its helper functions

--- settlement_clustering.py
+++ settlement_clustering.py
@@ -2,6 +2,8 @@
 A module for clustering settlements into administrative units.
 """
 
+from pyvis.network import Network
+
 
 def read_file(file_name: str) -> dict[str, dict[str, float]]:
     """
@@ -62,11 +64,55 @@ def command_line_interface():
     pass
 
 
-def visualize(clusters: list):
+def assign_color_to_clusters(clusters: list[dict]) -> None:
     """
-    Visualizes a weighted graph using the pyvis library.
+    Works in-place. Assigns a random color to every cluster of a parameter clusters.
 
-    :param clusters: list, The nodes divided to clusters.
+    :param clusters: list, A list of clusters to assign colors to.
     :return: None
     """
-    pass
+    for cluster in clusters:
+        cluster["color"] = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+
+def assign_color_to_nodes(clusters: list[dict]) -> dict[str, str]:
+    """
+
+    :param clusters: list, A list of clusters (dictionaries) with assigned color to each one.
+    :return: dict, A dictionary of every node as key and its color as value.
+    """
+    assigned_nodes = {}
+
+    for cluster in clusters:
+        for node in cluster["nodes"]:
+            assigned_nodes[node] = cluster["color"]
+
+    return assigned_nodes
+
+
+def visualize(graph: dict[str, dict[str, float]], colors: dict[str, str] = None) -> None:
+    """
+    Visualizes a weighted graph using the pyvis library. Optional parameter colors: if not None,
+    paints each node to its color. If you wish to call the function with this parameter, please
+    assign colors to your clusters list using the assign_color_to_clusters function and then
+    call this function like this:
+
+    visualize(graph, assign_color_to_nodes(your_clusters))
+
+    :param graph: dict, The graph to visualize.
+    :param colors: dict, A dictionary of every node as key and its color as value.
+    :return: None
+    """
+    net = Network(notebook=True)
+
+    added_nodes = []
+    for node, edges in graph.items():
+        net.add_node(node, color=colors[node] if colors else "blue")
+        added_nodes.append(node)
+        for neighbour, distance in edges.items():
+            if neighbour in added_nodes:
+                net.add_edge(node, neighbour, width=distance, length=distance)
+
+    net.force_atlas_2based()
+
+    net.show("graph.html")
