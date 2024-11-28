2dccdc01962402db62827f299cddd25699322c54
Roman Pempus <pempus.pn@ucu.edu.ua>
2024-11-28 20:17:46 +0200

    start developing command_line function, develop smooth_text function to improve usage of our product,

--- settlement_clustering.py
+++ settlement_clustering.py
@@ -1,8 +1,7 @@
 """
 A module for clustering settlements into administrative units.
 """
-
-
+import time
 def read_file(file_name: str) -> dict[str, dict[str, float]]:
     """
     Reads the graph from file.
@@ -59,7 +58,44 @@ def command_line_interface():
 
     :return: None
     """
-    pass
+    default_time_delay = 0.035
+    def smooth_text(text:str,delay):
+        """
+        function that smooth_texts text in terminal not instantly but with small time sleep
+        so that this text smooth_texting process can be beautiful
+        """
+        for i, char in enumerate(text):
+            if i == len(text)-1:
+                print(char,flush=True)
+                time.sleep(delay)
+            else:
+                print(char,end="",flush=True)
+                time.sleep(delay)
+    smooth_text("Hello, my dear friend!!\nThis program can help if you want to do some clustering with your data",default_time_delay)
+    smooth_text("Here is some main requirements:\nFile(with data to cluster) format should be .dot",default_time_delay)
+    smooth_text("Also we need your data to be like this:\ngraph\nA — B [distance]",default_time_delay)
+    smooth_text("B — C [distance]",default_time_delay)
+    smooth_text("A — C [distance]",default_time_delay)
+    smooth_text("Where 'A','B','C' - names of the nodes\ndistance - distance between those nodes",default_time_delay)
+    smooth_text("write path to file here",default_time_delay)
+    path_name = input("path to file:",)
+    file_format = path_name[-3:]
+    if file_format != "dot":
+        if_wrong_format_text = ("Sorry but format of file doesn't match the requirements, please pay attention to format of it and try again",)
+        return smooth_text(if_wrong_format_text,default_time_delay)
+    else:
+        check_file = check_file_content(path_name)
+        if check_file != "Wrong file (return of that function)":
+            smooth_text("Also please type number of clusters that you want to recieve. Number can be from 1 to 100. If it doesn't matter for you type \"0\"",default_time_delay)
+            number_of_clusters = input("Please type the number of clusters and 0 if it doesn't matter:")
+            if number_of_clusters == 0:
+                result_of_clustering = dbscan(read_file(path_name))
+                
+            elif number_of_clusters > 1 and number_of_clusters < 100:
+                result_of_clustering = kmedoids_clustering(read_file(path_name), number_of_clusters)
+        else:
+            if_wrong_file_text = "Sorry but your file does not match requirements for clustering, you may choose another file and try again"
+            return smooth_text(if_wrong_file_text,default_time_delay)
 
 
 def visualize(clusters: list):
