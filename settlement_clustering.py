bc80e7adbfc46a0e91172b390f1757e79d524026
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2024-12-02 16:37:32 +0200

    Add argparse as an option for running the script
    You can now run the script with arguments

--- settlement_clustering.py
+++ settlement_clustering.py
@@ -3,6 +3,9 @@ A module for clustering settlements into administrative units.
 """
 
 
+import argparse
+
+
 def read_file(file_name: str) -> dict[str, dict[str, float]]:
     """
     Reads the graph from file.
@@ -70,3 +73,45 @@ def visualize(clusters: list):
     :return: None
     """
     pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="Settlement clustering",
+        description='A module for clustering weighted graphs.',
+        epilog="If you are unsure of what to do, run this script without arguments.",
+        usage="settlement_clustering.py -f PATH [-n NUM_OF_CLUSTERS] [-v]")
+    parser.add_argument('-f', '--file', help="path to the file to read your graph from (required)",
+                        metavar="PATH", type=str)
+    parser.add_argument('-n', metavar='NUM_OF_CLUSTERS', type=int,
+                        help="the number of clusters to divide your graph into, blank to determine automatically",
+                        default=None)
+    parser.add_argument('-v', '--visualize', help="use to visualize the clustered graph",
+                        default=None, action='store_true')
+
+    args = parser.parse_args()
+
+    if not args.file and not args.n and not args.visualize:
+        command_line_interface()
+    else:
+        if not args.file:
+            parser.error("the following argument is required: -f/--file")
+
+        graph = read_file(args.file)
+
+        if not args.n:
+            clusters = dbscan(graph)
+        else:
+            if args.n < 0:
+                parser.error("argument -n: number of clusters cannot be less than zero.")
+            clusters = kmedoids_clustering(graph, args.n)
+
+        if args.visualize:
+            visualize(graph, clusters)
+        else:
+            pass
+            # print_clusters(clusters)
+
+
+if __name__ == "__main__":
+    main()
