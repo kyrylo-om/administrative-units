7a82b4c8adbe5185ae57ee6c9a10e907b69d61fd
Roman Pempus <pempus.pn@ucu.edu.ua>
2024-12-03 16:32:08 +0200

    update logic of wrong input, add using of louvare algorithm

--- settlement_clustering.py
+++ settlement_clustering.py
@@ -4,6 +4,7 @@ A module for clustering settlements into administrative units.
 import time
 import os
 import webbrowser
+import random
 def read_file(file_name: str) -> dict[str, dict[str, float]]:
     """
     Reads the graph from file.
@@ -107,7 +108,6 @@ def command_line_interface():
             else:
                 print(char,end="",flush=True)
                 time.sleep(delay)
-    
     #introduction of the program to user
     smooth_text("Hello, my dear friend!!\nThis program can help if you want to do some clustering with your data",default_time_delay)
     smooth_text("Here is some main requirements:\nFile(with data to cluster) format should be .dot",default_time_delay)
@@ -116,11 +116,11 @@ def command_line_interface():
     smooth_text("A — C [distance]",default_time_delay)
     smooth_text("Where 'A','B','C' - names of the nodes\ndistance - distance between those nodes\nIn addition, whenever you want to quit - just type \"quit\"",default_time_delay)
     smooth_text("write path to file here",default_time_delay)
+    #check file
     path_name = input("path to file:")
     graph = read_file(path_name)        
     check_file_content = validator(graph)
-    while isinstance(check_file_content(), str):
-        smooth_text("Sorry but your graph doesn't match the requirements. It has distances < 0 or is some node may be isolated so clustering makes no sense or there already is cluster",default_time_delay)
+    while isinstance(check_file_content, str):
         smooth_text("Please try again",default_time_delay)
         path_name = input("path to file:")
         if path_name == "quit":
@@ -130,39 +130,50 @@ def command_line_interface():
         if check_file_content == "quit":
             exit()
 
-            
-
     smooth_text("Also please type number of clusters that you want to recieve. Number can be from 1 to 100. If it doesn't matter for you type \"0\"",default_time_delay)
     number_of_clusters = input("Please type the number of clusters and 0 if it doesn't matter:")
+    while number_of_clusters != 0 and number_of_clusters < 1 and number_of_clusters > 100 and number_of_clusters != "quit" :
+        smooth_text("sorry but it seems you put invalid number of clusters, please try again",default_time_delay)
+        number_of_clusters = input("Please type the number of clusters and 0 if it doesn't matter:")
     if number_of_clusters == "quit":
         exit()
     #choosing algorithm depending on number of clusters
-    # if number_of_clusters == 0:
-    #     result_of_clustering = dbscan(graph)
-        
+    if number_of_clusters == 0:
+        smooth_text("Also you may choose which algorithm to choose\n>>>>>>k_medoids\n>>>>>>louvain",default_time_delay)
+        smooth_text("type \"k\" to use k_medoids, \"l\" to use louvain and \"n\" if it doesn't matter",default_time_delay)
+        alg_choice = input(smooth_text("choose algorithm:",default_time_delay))
+        while alg_choice != "l" and alg_choice != "n" and alg_choice != "k" and alg_choice != "quit":
+                smooth_text("sorry but your input is invalid, choose the algorithm by typing one english letter: l,k,n or quit to exit")
+                alg_choice = input(smooth_text("choose algorithm:",default_time_delay))
+        if alg_choice == "k":
+            result_of_clustering = kmedoids_clustering(graph)
+        elif alg_choice == "l":
+            result_of_clustering == louvain_algorithm(graph)
+        elif alg_choice == "n":
+            alg = 0
+            alg = random.randint(1,2)
+            if alg == 1:
+                result_of_clustering = kmedoids_clustering(graph)
+            else:
+                result_of_clustering == louvain_algorithm(graph)
+        elif alg_choice == "quit":
+            exit() 
     if number_of_clusters > 1 and number_of_clusters < 100:
         result_of_clustering = []
         result_of_clustering = kmedoids_clustering((graph), number_of_clusters)
-    while number_of_clusters != 0 and number_of_clusters < 1 and number_of_clusters > 100: 
-        smooth_text("sorry but it seems you put invalid number of clusters, please try again",default_time_delay)
-        number_of_clusters = input("Please type the number of clusters and 0 if it doesn't matter:")
-        if number_of_clusters == "quit":
-            exit()
     # visualisation of result
     smooth_text("Clustering is done! type \"term\" if you want to see the results in terminal and \"browser\"", default_time_delay)
     smooth_text("if you want to see the results using browser",default_time_delay)
     vis_choice = input(smooth_text("way of visualisation:",default_time_delay))
+    while vis_choice != "browser" and vis_choice != "term":
+        vis_choice = input(smooth_text("command not found, please choose the way to see the results:",default_time_delay))
     if vis_choice == "quit":
         exit()
-    if vis_choice == "term":
+    elif vis_choice == "term":
         visualisation_in_terminal(result_of_clustering)
     elif vis_choice == "browser":
         webbrowser.open(f"{os.getcwd()}\\graph.html")
         return smooth_text("Thank you for using our program! Have a great day!",default_time_delay)
-    while vis_choice != "browser" and vis_choice != "term":
-        vis_choice(smooth_text("command not found, please choose the way to see the results:",default_time_delay))
-        if vis_choice == "quit":
-            exit()
 
 
 
